bb72f453e1bc46683f6c496d93cdcfb5787d0188
joaovcostas <jvc22102003@gmail.com>
2024-03-18 20:59:23 -0300

    feat: create first api route

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/teste', () => {
+    return 'hello world'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('🔥 HTTP server running')
+})
