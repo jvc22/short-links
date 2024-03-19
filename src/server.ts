e4e8f8ec5fb03d92ad2b0146ddff21fedbc402da
joaovcostas <jvc22102003@gmail.com>
2024-03-18 22:03:57 -0300

    feat: complete /links route and treat error messages

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,44 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { z } from 'zod'
+import { sql } from './lib/postgres'
+import { PostgresError } from 'postgres'
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return 'hello world'
+app.post('/links', async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  })
+
+  const { code, url } = createLinkSchema.parse(request.body)
+
+  try {
+    const result = await sql`
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id
+  `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id })
+  } catch (err) {
+    if (err instanceof PostgresError) {
+      if (err.code === '23505') {
+        return reply.status(400).send({ message: 'Duplicated code.' })
+      }
+    }
+
+    console.error(err)
+    return reply.status(500).send({ message: 'Internal error.' })
+  }
 })
 
-app.listen({
+app
+  .listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('🔥 HTTP server running')
-})
+  })
